cfc8dfdf744bc0dd9a99155ce8dea1e4c654d99e
keisuke_yoza <yoza_k@occ.co.jp>
2022-06-06 12:49:40 +0900

    画面遷移（掲示板、Goodカード一覧、受け取ったコメント一覧、送ったコメント一覧）

---
+++ ThanksCardClient/ViewModels/BulletinBoardViewModel.cs
@@ -0,0 +1,34 @@
+﻿#nullable disable
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanksCardClient.Model;
+using ThanksCardClient.Services;
+
+namespace ThanksCardClient.ViewModels
+{
+    internal class BulletinBoardViewModel : BindableBase
+    {
+        private readonly IRegionManager regionManager;
+        public BulletinBoardViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+
+        #region  BackCommand //1つ前のページへ画面遷移
+        private DelegateCommand _BackCommand;
+        public DelegateCommand BackCommand =>
+            _BackCommand ?? (_BackCommand = new DelegateCommand(ExecuteBackCommand));
+        void ExecuteBackCommand()
+        {
+            this.regionManager.RequestNavigate("ContentRegion", nameof(Views.MainMenu));
+        }
+        #endregion
+    }
+}
