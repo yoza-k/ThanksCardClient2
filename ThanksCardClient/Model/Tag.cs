d0c602c32f03c2fa00149375a3b14aacc82cc1d1
keisuke_yoza <yoza_k@occ.co.jp>
2022-06-08 11:30:31 +0900

    ThanksCardCreate 部署プルダウン

--- ThanksCardClient/Model/Tag.cs
+++ ThanksCardClient/Model/Tag.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ThanksCardClient.Services;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ThanksCardClient.Models
 {
@@ -19,6 +21,7 @@ namespace ThanksCardClient.Models
             set { SetProperty(ref _Id, value); }
         }
         #endregion
+        
         #region CdProperty
         private long _Cd;
         public long Cd
@@ -27,6 +30,7 @@ namespace ThanksCardClient.Models
             set { SetProperty(ref _Cd, value); }
         }
         #endregion
+        
         #region NameProperty;
         private string _Name;   
         public string Name
@@ -35,5 +39,54 @@ namespace ThanksCardClient.Models
             set { SetProperty(ref _Name, value); }
         }
         #endregion
+
+        //#region ThanksCardTagsProperty
+        //private List<ThanksCardTag> _ThanksCardTags;
+        //public List<ThanksCardTag> ThanksCardTags
+        //{
+        //    get { return _ThanksCardTags; }
+        //    set { SetProperty(ref _ThanksCardTags, value); }
+        //} 
+        //#endregion
+
+        #region SelectedProperty
+        private bool _Selected;
+
+        // JSON シリアライズから除外する
+        [JsonIgnore]
+        public bool Selected
+        {
+            get { return _Selected; }
+            set { SetProperty(ref _Selected, value); }
+        }
+        #endregion
+
+        public async Task<List<Tag>> GetTagsAsync()
+        {
+            IRestService rest = new RestService();
+            List<Tag> tags = await rest.GetTagsAsync();
+            return tags;
+        }
+
+        public async Task<Tag> PostTagAsync(Tag tag)
+        {
+            IRestService rest = new RestService();
+            Tag createdTag = await rest.PostTagAsync(tag);
+            return createdTag;
+        }
+
+        public async Task<Tag> PutTagAsync(Tag tag)
+        {
+            IRestService rest = new RestService();
+            Tag updatedTag = await rest.PutTagAsync(tag);
+            return updatedTag;
+        }
+
+        public async Task<Tag> DeleteTagAsync(long Id)
+        {
+            IRestService rest = new RestService();
+            Tag deletedTag = await rest.DeleteTagAsync(Id);
+            return deletedTag;
+        }
     }
 }
--- ThanksCardClient/ViewModels/ThanksCardCreateViewModel.cs
+++ ThanksCardClient/ViewModels/ThanksCardCreateViewModel.cs
@@ -4,20 +4,121 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ThanksCardClient.Model;
+using ThanksCardClient.Models;
 using ThanksCardClient.Services;
+
 namespace ThanksCardClient.ViewModels
 {
-    internal class ThanksCardCreateViewModel : BindableBase
+    internal class ThanksCardCreateViewModel : BindableBase, INavigationAware
     {
         private readonly IRegionManager regionManager;
+
+        #region ThanksCardProperty
+        private ThanksCard _ThanksCard;
+        public ThanksCard ThanksCard
+        {
+            get { return _ThanksCard; }
+            set { SetProperty(ref _ThanksCard, value); }
+        }
+        #endregion
+
+        #region FromUsersProperty
+        private List<User> _FromUsers;
+        public List<User> FromUsers
+        {
+            get { return _FromUsers; }
+            set { SetProperty(ref _FromUsers, value); }
+        }
+        #endregion
+
+        #region ToUsersProperty
+        private List<User> _ToUsers;
+        public List<User> ToUsers
+        {
+            get { return _ToUsers; }
+            set { SetProperty(ref _ToUsers, value); }
+        }
+        #endregion
+
+        #region DepartmentsProperty
+        private List<Department> _Departments;
+        public List<Department> Departments
+        {
+            get { return _Departments; }
+            set { SetProperty(ref _Departments, value); }
+        }
+        #endregion
+
+        #region TagsProperty
+        private List<Tag> _Tags;
+        public List<Tag> Tags
+        {
+            get { return _Tags; }
+            set { SetProperty(ref _Tags, value); }
+        }
+        #endregion
+
         public ThanksCardCreateViewModel(IRegionManager regionManager)
         {
             this.regionManager = regionManager;
         }
+
+        // この画面に遷移し終わったときに呼ばれる。
+        // それを利用し、画面表示に必要なプロパティを初期化している。
+        public async void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            this.ThanksCard = new ThanksCard();
+
+            if (SessionService.Instance.AuthorizedUser != null)
+            {
+                this.FromUsers = await SessionService.Instance.AuthorizedUser.GetUsersAsync();
+                this.ToUsers = this.FromUsers;
+            }
+
+            var tag = new Tag();
+            this.Tags = await tag.GetTagsAsync();
+
+            var dept = new Department();
+            this.Departments = await dept.GetDepartmentsAsync();
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            //throw new NotImplementedException();
+        }
+
+        #region FromDepartmentsChangedCommand
+        private DelegateCommand<long?> _FromDepartmentsChangedCommand;
+        public DelegateCommand<long?> FromDepartmentsChangedCommand =>
+            _FromDepartmentsChangedCommand ?? (_FromDepartmentsChangedCommand = new DelegateCommand<long?>(ExecuteFromDepartmentsChangedCommand));
+
+        async void ExecuteFromDepartmentsChangedCommand(long? FromDepartmentId)
+        {
+            this.FromUsers = await SessionService.Instance.AuthorizedUser.GetDepartmentUsersAsync(FromDepartmentId);
+        }
+        #endregion
+
+        #region ToDepartmentsChangedCommand
+        private DelegateCommand<long?> _ToDepartmentsChangedCommand;
+        public DelegateCommand<long?> ToDepartmentsChangedCommand =>
+            _ToDepartmentsChangedCommand ?? (_ToDepartmentsChangedCommand = new DelegateCommand<long?>(ExecuteToDepartmentsChangedCommand));
+
+        async void ExecuteToDepartmentsChangedCommand(long? ToDepartmentId)
+        {
+            this.ToUsers = await SessionService.Instance.AuthorizedUser.GetDepartmentUsersAsync(ToDepartmentId);
+        }
+        #endregion
+
         #region  BackCommand //1つ前のページへ戻る
         private DelegateCommand _BackCommand;
         public DelegateCommand BackCommand =>
@@ -27,5 +128,6 @@ namespace ThanksCardClient.ViewModels
             this.regionManager.RequestNavigate("ContentRegion", nameof(Views.MainMenu));
         }
         #endregion
+
     }
 }
